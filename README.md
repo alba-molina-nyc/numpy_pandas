afc49e3dab22b65658ce57fab64349d6bb67a0c0
Username <albavmolinanyc@gmail.com>
2022-06-17 14:01:16 -0400

    reads headers and access pokemons by name

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ### 1
 
 load data into pandas
+
+you can change the demilimeter inside the read_csv() function to whatever actually separates each iteam
+
+### 2
+
+read the headers
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('pokemon_data.csv')
-print(df)
+df = pd.read_csv('pokemon_data.csv', delimiter=',')
+# print(df)
 
+# print(df.columns)                               # reads columns
+
+# print(df['Name'])                               # only get the name of the pokemons
+
+print(df['Name'][0:5])                            # use slicing to only get first 5
